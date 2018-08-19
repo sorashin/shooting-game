bf44dedd4608f8bd3e1081bd14d2a598af9d9052
Shintaro Nakajima <nakamajin52@gmail.com>
2018-08-19 21:26:30 +0900

    first commit

---
+++ shooting-game/Assets/Script/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float speed = 10;
+	// Use this for initialization
+	void FixedUpdate () {
+		// bool isRight = OVRInput.Get(OVRInput.Button.Right);
+		Vector2 touchPadPt = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad);
+		float x = touchPadPt.x;
+		float z = touchPadPt.y;
+
+		Rigidbody rigidbody = GetComponent<Rigidbody>();
+		rigidbody.AddForce(x, 0, z);
+
+		rigidbody.AddForce(x * speed, 0, z * speed);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ shooting-game/Assets/Script/RightHandAnchor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightHandAnchor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+			if(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger)){
+				GameObject bullet = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+				bullet.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+				bullet.transform.position = transform.position;
+				Rigidbody body = bullet.AddComponent <Rigidbody>();
+				Vector3 force = transform.forward * 20.0f;
+				body.AddForce(force, ForceMode.Impulse);
+				body.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+				Destroy(bullet, 1.0f);
+			}
+
+	}
+}
